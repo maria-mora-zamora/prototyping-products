bb3876c7c1938e6c8312b64edf363ee77f3d8ffe
Kika <mariaangelica.morazamora@alumni.esade.edu>
2026-03-12 12:32:30 +0100

    Fixed start.sh code

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
 
-# Start Ollama in the background
 ollama serve &
-
-# Wait for Ollama to respond
 sleep 5
 
-# Download the model if it's not already downloaded
 ollama pull llama3.2:3b
 
-# Launch Streamlit
-streamlit run app-LLM.py --server.port=8501 --server.address=0.0.0.0
+streamlit run app-LLM.py --server.port=${PORT:-8501} --server.address=0.0.0.0
